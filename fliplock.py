f86373f8c8a1ad1f7240907c8d47be28429b31e1
Friedrich Albert Kyuri <friedrichak42@gmail.com>
2023-11-10 11:45:30 +0100

    prettify and add atexit

--- fliplock.py
+++ fliplock.py
@@ -1,53 +1,95 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import Flask
+import atexit
 import os
+from pathlib import Path
+from typing import Union
+
+from flask import Flask
+from loguru import logger
+
+DEVICES_TO_GRAB = ["/dev/input/event3", "/dev/input/event9"]
+
+# Create log folder
+Path("./logs").mkdir(exist_ok=True)
+
+
+# Set up the logger
+logger.remove()
+logger.add("./logs/latest.log", rotation="10 MB", retention="3 days", compression="zip", enqueue=True, serialize=True, level="DEBUG")
 
 ENABLED = 'enabled'
 DISABLED = 'disabled'
 
-Icon = "./switch-on.png"
-Icoff = "./switch-off.png"
 fconfig = "tabmode.conf"
 
 global state
-state = ''
+# disabled by default
+state = DISABLED
 
-if not os.path.isfile(fconfig):
-    print("Creating config file")
+@logger.catch()
+def get_state() -> Union[ENABLED, DISABLED]:
+    global state
+
+    if not os.path.isfile(fconfig):
+        logger.debug("Creating config file")
+        with open(fconfig, 'w') as config_file:
+            config_file.write(state)
+        return state
+    else:
+        with open(fconfig, 'r') as config_file:
+            state = config_file.read().strip()
+        return state
+
+@logger.catch()
+def save_state(state: Union[ENABLED, DISABLED]):
     with open(fconfig, 'w') as config_file:
-        config_file.write(ENABLED)
-    state = ENABLED
-else:
-    with open(fconfig, 'r') as config_file:
-        state = config_file.read().strip()
-    print(f"keyboard is: {state}")
+        config_file.write(state)
+
+@logger.catch()
+def block_devices():
+    for dev in DEVICES_TO_GRAB:
+        os.system(f'evtest --grab {dev} &')
+
+@logger.catch()
+def unblock_devices():
+    os.system('pkill evtest')
+
+@logger.catch()
+def toggle_state(current_state: Union[ENABLED, DISABLED]) -> Union[ENABLED, DISABLED]:
+    global state
+    if current_state == DISABLED:
+        logger.debug("enable...")
+        block_devices()
+        state = ENABLED
+    elif current_state == ENABLED:
+        logger.debug("disable...")
+        unblock_devices()
+        state = DISABLED
+
+    save_state(state)
+    return state
+
+atexit.register(lambda: toggle_state(DISABLED))
 
 app = Flask(__name__)
 
 @app.route('/')
+@logger.catch()
 def index():
     return 'fliplock api'
 
 @app.route('/toggle')
+@logger.catch()
 def toggle():
     global state
-    print(state)
-    if state == DISABLED:
-        print("enable...")
-        os.system('evtest --grab /dev/input/event3 &')
-        os.system('evtest --grab /dev/input/event9 &')
-        with open(fconfig, 'w') as config_file:
-            config_file.write(ENABLED)
-        state = ENABLED
 
-    elif state == ENABLED:
-        print("disable...")
-        os.system('pkill evtest')
-        with open(fconfig, 'w') as config_file:
-            config_file.write(DISABLED)
-        state = DISABLED
+    # flip state
+    logger.info(f"state is {state}. flipping...")
+    state = toggle_state(ENABLED if state == DISABLED else DISABLED)
+    logger.info(f"flipped state to {state}")
 
-    return 'success'
+    return f"Tablet mode is {state}"
 
-app.run(host='localhost', port=4244)
+if __name__ == "__main__":
+    app.run(host='localhost', port=4244)
