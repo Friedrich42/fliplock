982f8b822cb3a659796521ee4df9772ff8b4725e
Friedrich Albert Kyuri <friedrichak42@gmail.com>
2023-11-10 08:43:36 +0100

    prettify output

--- tablet.sh
+++ tablet.sh
@@ -1,1 +1,2 @@
-curl localhost:4244/toggle && notify-send -i /opt/fliplock/tablet.png "Toggle Tablet Mode"
+output=$(curl -sS localhost:4244/toggle 2>&1)
+notify-send -i /opt/fliplock/tablet.png "Toggle Tablet Mode" "$output"
